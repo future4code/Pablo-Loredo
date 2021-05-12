7264f036cc6fd11b796f974311f9aec39cc54ea3
Pablo Loredo <ploredo17@gmail.com>
2021-05-12 16:13:40 -0300

    semana 2 aula 09 inc

---
+++ semana2/loops/index.js
@@ -0,0 +1,85 @@
+/* let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor) 
+
+
+0 1 2 3 4 5
+
+
+*/
+
+/* const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+} 
+
+19, 21, 23, 25, 27, 30
+Sim, indexOf
+
+*/
+
+ /* const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+let quantidadeAtual = 0
+while(quantidadeAtual < quantidadeTotal){
+  let linha = ""
+  for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+    linha += "0"
+  }
+  console.log(linha)
+  quantidadeAtual++
+}  
+
+Ele vai imprimir 4 zeros 
+*/
+
+//---------------------//---------------------//
+
+// EX1
+ /* let bichinho = () => {
+ let bichinhos = Number(prompt("Quantos bichinhos voce tem?"))
+ let quantidadeBichinho = []
+if (bichinhos === 0) {
+    console.log("Que pena! Você pode adotar um pet!")
+} else { for (let i= 0;i < bichinhos; i++)
+    {let quantidadeBichinhos1 = prompt("Diga o nome dos seus pets")
+     quantidadeBichinho = quantidadeBichinho + quantidadeBichinhos1
+     console.log(quantidadeBichinho)
+}
+
+}
+return
+ }
+ */
+//bichinho()
+//-------------------//--------------------//
+
+// EX2
+ let arrayOriginal = [10,20,37,48,50]
+/* const imprimeArray1 = () =>{ 
+ for (let i = 0; i < arrayOriginal.length; i++){
+     console.log(arrayOriginal[i])
+ }
+for (let i = 0; i < arrayOriginal.length;i++){
+    console.log(arrayOriginal[i] / 10)
+}
+} */ 
+
+/* imprimeArray1() */
+let ar =[]
+const imprimeArray2 = () =>{
+    for (var i = 0; i < arrayOriginal.length; i++) {
+        if(i % 2 === 0) { 
+            ar.push(arrayOriginal[i])
+            console.log(arrayOriginal[i])
+        }
+    }
+     
+     }
+ 
+
+imprimeArray2()
+
